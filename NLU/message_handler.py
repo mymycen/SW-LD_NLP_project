64a21e065f8342f6c68c1fe023c6b001122ac358
lkbuening <l.kleinebuening@campus.tu-berlin.de>
2018-02-03 16:24:52 +0100

    Removed confidence check

--- NLU/message_handler.py
+++ NLU/message_handler.py
@@ -21,12 +21,12 @@ class MessageHandler:
         # Parse user input
         nlu_json_response = self.interpreter.parse(message)
 
-        confidence = nlu_json_response['intent']['confidence']
+        print(nlu_json_response)
         entities = nlu_json_response['entities']
         subject = None
         predicate = None
 
-        if confidence < 0.7 or len(entities) < 2:
+        if len(entities) < 2:
             resp = requests.get('http://localhost:8080/nlp?message=' + message)
             if resp.status_code != 200:
                 raise resp.status_code
