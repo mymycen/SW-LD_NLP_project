c146d3324f9f12f03abe8ec59a87a8d95db79048
Venkat <subbaraocse9@gmail.com>
2018-02-03 17:59:22 +0100

    Solr Index code

---
+++ SolrIndex/README.md
@@ -0,0 +1,19 @@
+# Search index for semantic data
+
+1. cd to repo folder  
+``cd SolrSearch``
+2. Run all the below commands  
+ ```
+ wget http://archive.apache.org/dist/lucene/solr/7.1.0/solr-7.1.0.tgz
+ tar -zxvf solr-7.1.0.tgz
+ mv search.tar.gz solr-7.1.0/server/solr
+ cd solr-7.1.0/server/solr
+ tar -zxvf search.tar.gz
+ rm search.tar.gz
+ ```
+3. Run `(cd ../../ && ./bin/solr start -p 8983)`
+4. Load the repository into IDE.
+5. Locate Main.java and run the class
+6. Now you must be able to send and retrieve information about Berlin on
+
+http://localhost:5050/search/get?search=what is the capital of Germany?
---
+++ SolrIndex/src/main/java/com/swld/Main.java
@@ -0,0 +1,63 @@
+package com.swld;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Main class.
+ *
+ */
+public class Main {
+
+
+    // Base URI the Grizzly HTTP server will listen on
+    public static String BASE_URI = "http://localhost:5050/";
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        // create a resource config that scans for JAX-RS resources and providers
+        // in com.underdog.jersey.grizzly package
+        final ResourceConfig rc = new ResourceConfig().packages("com.swld").register(new CORSFilter());
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void readArgs(String[] args){
+        try {
+            if (args.length == 1) {
+                int port = Integer.parseInt(args[0]);
+                BASE_URI = "http://localhost:" + port + "/de/";
+            } else if (args.length == 2) {
+                String base = args[0];
+                int port = Integer.parseInt(args[1]);
+                BASE_URI = "http://" + base + ":" + port + "/de/";
+            }
+        } catch(NumberFormatException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Main method.
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        readArgs(args);
+        final HttpServer server = startServer();
+        System.out.println("Started server "+BASE_URI);
+        System.out.println("hit enter to stop");
+        System.in.read();
+        server.stop();
+    }
+}
+
+
