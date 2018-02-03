cd2d7334b55a6ced74eb43de751795212db0d2a4
Pkanjan37 <p.pichaya@outlook.com>
2018-02-03 22:06:40 +0700

    Edit spring boots

--- initBackend/src/main/java/initBackend/test.java
+++ initBackend/src/main/java/initBackend/test.java
@@ -1,14 +1,12 @@
 package initBackend;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class test {
     public static void main(String[] args) {
-        SpringApplication.run(test.class, args);
-        //initBackend a = new initBackend("USA","capl","What is the capl of USA?");
-        //String x = a.lookup(false);
-        //System.out.println(x+"  <<<");
+        //SpringApplication.run(test.class, args);
+        initBackend a = new initBackend("","","What is the capital of Germany?");
+        String x = a.lookup();
+        System.out.println(x+"  <<<");
     }
 }
