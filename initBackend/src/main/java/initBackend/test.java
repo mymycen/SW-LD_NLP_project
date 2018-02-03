90819f75406ecc9113e1819797c06701ec61947d
lkbuening <l.kleinebuening@campus.tu-berlin.de>
2018-02-03 16:26:22 +0100

    changed comments in test.java

--- initBackend/src/main/java/initBackend/test.java
+++ initBackend/src/main/java/initBackend/test.java
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class test {
     public static void main(String[] args) {
-        //SpringApplication.run(test.class, args);
-        initBackend a = new initBackend("","","What is the capital of Germany?");
-        String x = a.lookup();
-        System.out.println(x+"  <<<");
+        SpringApplication.run(test.class, args);
+        //initBackend a = new initBackend("","","What is the capital of Germany?");
+        //String x = a.lookup();
+        //System.out.println(x+"  <<<");
     }
 }
